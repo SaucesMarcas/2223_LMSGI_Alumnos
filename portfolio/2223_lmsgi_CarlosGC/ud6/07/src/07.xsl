baea98cc9b2f8df33f124026bde0374f6ad19a24
CarlosGarcia <carlogarcicachon@gmail.com>
2023-06-01 13:54:34 +0200

    UD06_07

--- portfolio/2223_lmsgi_CarlosGC/ud6/07/src/07.xsl
+++ portfolio/2223_lmsgi_CarlosGC/ud6/07/src/07.xsl
@@ -1,33 +1,28 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
-    <xsl:output method="text"/>
-    <xsl:template match="/network">
-        <xsl:text>network:</xsl:text>
-        <xsl:text>
-   version: </xsl:text><xsl:value-of select="version"/>
-        <xsl:apply-templates select="interface"/>
-    </xsl:template>
-    
-    <xsl:template match="interface">
-        <xsl:text>
-   interface:</xsl:text>
-        <xsl:text>
-      ethernets: </xsl:text><xsl:value-of select="ethernets"/>
-        <xsl:text>
-      dhcp4: </xsl:text><xsl:value-of select="dhcp4"/>
-        <xsl:text>
-      address: </xsl:text><xsl:value-of select="concat('[',address,'/24]')"/>
-        <xsl:text>
-      gateway: </xsl:text><xsl:value-of select="gateway"/>
-        <xsl:text>
-      nameservers: [</xsl:text>
-        <xsl:apply-templates select="nameservers/address"/>]
-    </xsl:template>
-    
-    <xsl:template match="nameservers/address">
-        <xsl:value-of select="current()"/>
-        <xsl:if test="position()!=last()">
-            <xsl:text>,</xsl:text>
-        </xsl:if>
-    </xsl:template>
+    <xsl:output method="xml"/>
+    <xsl:template match="/daw">
+        
+        <xsl:element name="daw">
+            <xsl:attribute name="curso">
+                <xsl:value-of select="@curso"/>
+            </xsl:attribute>
+            <xsl:for-each select="alumnos/alumno">
+                <xsl:element name="alumno">
+                    <xsl:attribute name="expediente">
+                        <xsl:value-of select="expediente"/>
+                    </xsl:attribute>
+                    <xsl:for-each select="//calificacion[@expediente=current()/expediente]">
+                        <xsl:element name="nota">
+                            <xsl:attribute name="sigla">
+                                <xsl:value-of select="@sigla"/>
+                            </xsl:attribute>
+                            <xsl:value-of select="round(sum(nota[.!'NP')div 3)"/>
+                        </xsl:element>
+                    </xsl:for-each>
+                </xsl:element>
+            </xsl:for-each>                
+        </xsl:element>
+  
+   </xsl:template>
 </xsl:stylesheet>
--- portfolio/BaseDatos/src/main/java/es/sauces/basedatos/BaseDatos.java
+++ portfolio/BaseDatos/src/main/java/es/sauces/basedatos/BaseDatos.java
@@ -8,9 +8,11 @@ package es.sauces.basedatos;
 import static es.sauces.basedatos.BaseDatos.createEquipo;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 /**
  *
@@ -44,7 +46,11 @@ public class BaseDatos {
             
             stmt = conexion.createStatement();
             stmt.executeUpdate("CREATE DATABASE IF NOT EXISTS BD10");
-            createEquipo(conexion,"BD10");
+            //createEquipo(conexion,"BD10");
+            //cargarEquipo(conexion,"BD10");
+            //verEquipo(conexion,"BD10");
+            //clientesPorRegion(conexion,"ebanca");
+            clientesPorRegionYApellido(conexion,"ebanca");
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
@@ -60,4 +66,71 @@ public class BaseDatos {
             stmt = con.createStatement();
             stmt.executeUpdate(consulta);
         }
+    
+    public static void cargarEquipo(Connection con, String nombreBD) throws SQLException {
+            Statement stmt = null;
+            try {
+                stmt = con.createStatement();
+            stmt.executeUpdate("insert into " + nombreBD + ".EQUIPOS VALUES (" + "1,'ESTEPONA','MONTERROSO','ESTEPONA','MALAGA','29680')");
+            stmt.executeUpdate("insert into " + nombreBD + ".EQUIPOS VALUES (" + "2,'ALCORCON','SANTO DOMINGO','ALCORCON','MADRID','28924')");
+            stmt.executeUpdate("insert into " + nombreBD + ".EQUIPOS VALUES (" + "3,'PORCUNA','SAN CRISTOBAL','PORCUNA','JAEN','23790')");
+            } catch(SQLException e) {
+                System.out.println(e.getMessage());
+            }
+    }
+    
+    public static void verEquipo(Connection con, String nombreBD) throws SQLException {
+            Statement stmt = null;
+            String consulta = "SELECT * FROM " + nombreBD + ".EQUIPOS";
+            try {
+                stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(consulta);
+                while(rs.next()) {
+                    int idEquipo = rs.getInt("IDEQUIPO");
+                    String nombre = rs.getString("NOMBRE");
+                    String estadio = rs.getString("ESTADIO");
+                    String poblacion = rs.getString("POBLACION");
+                    String provincia = rs.getString("PROVINCIA");
+                    int codigoPostal = rs.getInt("CODPOSTAL");
+                    System.out.println(idEquipo + " " + nombre + " " + estadio + " " + poblacion + " " + provincia + " " + codigoPostal);
+                }
+                
+            } catch(SQLException e) {
+                System.out.println(e.getMessage());
+            }
+    }
+    
+    public static void clientesPorRegion(Connection con, String nombreBD) throws SQLException{
+        String consulta="select count(codigo_cliente) from "+ nombreBD + ".clientes "+
+                "where region=?";
+        try(PreparedStatement ps = con.prepareStatement(consulta)) {
+            Scanner teclado = new Scanner(System.in);
+            System.out.println("introduce la region: ");
+            String region=teclado.nextLine();
+            ps.setString(1,region);
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+                System.out.println(rs.getInt(1));
+            }
+        }
+    }
+    
+    public static void clientesPorRegionYApellido(Connection con, String nombreBD) throws SQLException{
+        String consulta="select count(codigo_cliente) from "+ nombreBD + ".clientes "+
+                "where region=? and (LOWER (nombre) LIKE '?%')";
+        try(PreparedStatement ps = con.prepareStatement(consulta)) {
+            Scanner teclado = new Scanner(System.in);
+            System.out.println("introduce la region: ");
+            String region=teclado.nextLine();
+            System.out.println("introduce la primera letra del apellido: ");
+            String letra=teclado.nextLine();
+            ps.setString(1,region);
+            ps.setString(2, letra);
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+                System.out.println(rs.getInt(1));
+            }
+        }
+    }
+    
 }
